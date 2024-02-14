c40703c6c42c1b04e6cbf40098304d7a84fe4949
dnwn1211 <dnwn3027@gmail.com>
2024-02-14 09:38:04 +0900

    step_11
    step_11 clear

--- step_11/07.py
+++ step_11/07.py
@@ -2,11 +2,10 @@
 #알고리즘 수업 - 점근적 표기 1
 
 a1, a0 = map(int, input().split())
-
 c = int(input())
 n0 = int(input())
 
-if abs(a0) <= abs(n0 * (c - a1)):
+if a1*n0 + a0 <= c*n0 and a1 <= c:
     print(1)
 else:
     print(0)
